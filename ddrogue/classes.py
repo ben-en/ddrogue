fb2c17b28ded7a2f21a9ecfdb10d5317208b9456
Ben Ennis <nwbenennis@gmail.com>
2016-09-29 12:30:44 -0700

    update Fighter

--- ddrogue/classes.py
+++ ddrogue/classes.py
@@ -1,13 +1,17 @@
 from .mechanics.stats import HIGH_BAB, GOOD_SAVE, BAD_SAVE, compile_saves
 
 
-class Fighter:
+class Fighter(object):
     def __init__(self):
-        self.skill_progression = 2  # implies "int+2"
+        self.class_skills = 'climb craft handle animal intimidate '.split() + \
+                            'knowledge profession ride survival swim'.split()
+        self.knowledges = 'dungeoneering engineering'.split()
+
         self.hd = '1d10'
         self.bab = HIGH_BAB
         self.saves = compile_saves(GOOD_SAVE, BAD_SAVE, BAD_SAVE)
-        self.init_specials()
+        self.skill_progression = 2  # implies "int+2"
+        self.base_gold = '5d6'  # implies roll(5d6) * 10gp
 
     def init_specials(self):
         """ Should add special abilities, eg fighter's bonus feat every level
