d2507215010451b3ac59e765fa0d3abdc6936951
Ben Ennis <nwbenennis@gmail.com>
2016-09-29 12:32:37 -0700

    update Goblin

--- ddrogue/npc.py
+++ ddrogue/npc.py
@@ -1,7 +1,9 @@
 from pygame.sprite import Sprite, Group
 
 from .dice import roll
-from .mechanics.stats import Stat
+from .mechanics.stats import Stat, StatBlock
+from .mechanics.sizes import Small
+from .mechanics.weapons import Fist
 
 
 BLACK = 0, 0, 0
@@ -10,26 +12,29 @@ BLUE = 0, 255, 0
 WIDTH = HEIGHT = 32  # TODO only define this once
 
 
-class Goblin:
+class Goblin(Sprite):
     def __init__(self, image):
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
+        Sprite.__init__(self)
         self.image = image
         self.rect = image.get_rect()
         self.stats = self.init_stats()
-        self.hp = roll('1d4') + self.stats['con'].bonus
-        self.ac = 10 + self.stats['dex'].bonus
+        self.hp = roll('1d4') + self.stats.con.bonus
+        self.ac = 10 + self.stats.dex.bonus
+        self.bab = 1
+        self.size = Small()
+        self.weapons = [Fist(self)]
+        self.equipped = 0
         # TODO add armor and size values
 
     def init_stats(self):
-        stats = {
-            'str': Stat(13, 1),
-            'dex': Stat(14, 2),
-            'con': Stat(12, 1),
-            'wis': Stat(11, 0),
-            'cha': Stat(8, -1),
-            'int': Stat(9, -1),
-        }
+        stats = StatBlock(
+            str=Stat(13, 1),
+            dex=Stat(14, 2),
+            con=Stat(12, 1),
+            int=Stat(11, 0),
+            wis=Stat(8, -1),
+            cha=Stat(9, -1),
+        )
         return stats
 
 
