6915ff509a1779765f0921e56c3bae99d9747fe5
Ben Ennis <ben@outernet.is>
2016-10-05 21:06:16 -0700

    night's musings on character objects

--- ddrogue/mechanics/classes.py
+++ ddrogue/mechanics/classes.py
@@ -4,6 +4,55 @@ from .bab import HIGH_BAB
 from .saves import GOOD_SAVE, BAD_SAVE, compile_saves
 
 
+class Character(object):
+    def __init__(self, name, cclass, race, stats, interactive=False):
+        self.name = name
+        self.cclass = cclass
+        self.race = race
+        self.stats = stats
+
+        # Setup null attributes with appropriate typed info
+        self.hp = 0
+        self.bab = [0]
+        self.saves = 0, 0, 0
+        self.initiative = 0
+        self.ac = (10, 10, 10)
+        self.sr = 0
+        self.cmb = 0
+        self.cmd = 0
+        self.stats = StatBlock(*(Stat((10, 0)) for x in range(6)))
+        self.skills = SkillBlock(*(0 for x in SKILL_LIST))
+        self.feats = {
+            'levelup': [],
+            'combat': [],
+            'free': [],
+            'persistent': [],
+        }
+
+        self.abilities = {
+            'levelup': [],
+            'combat': [],
+            'free': [],
+            'persistent': [],
+        }
+        self.abilities.extend(race.abilities)
+        self.equipment = []
+        self.backpack = []
+        self.gold = 0
+
+        self.level = 0
+        self = self.levelup(interactive=interactive)
+
+    def levelup(self, interactive=False):
+        """ level the character up and return a new Character object """
+        new = self.copy()
+        new.level += 1
+        new.hp += (roll(self.cclass.hd) + self.stats.con.bonus)
+        new.bab = self.cclass.bab[new.level]
+        new.abilities += self.cclass.features[new.level]
+        return new
+
+
 BaseClass = namedtuple('BaseClass', [
     'hd',           # hit dice rolled for HP each level
     'bab',          # base attack bonus for the class
--- ddrogue/mechanics/skills.py
+++ ddrogue/mechanics/skills.py
@@ -1,11 +1,38 @@
+from collections import namedtuple
 
 
-SKILL_LIST = (
-    'acrobatics appraise artist bluff climb craft device diplomacy disable'
-    'disguise escape fly handle-animal heal intimidate knowledge linguistics'
-    'perception perform profession ride sense-motive sleight-of-hand'
-    'spellcraft stealth survival swim use-magic-device'
-).split()
+Skill = namedtuple('Skill', ['name', 'stat', 'enc'])
+
+SKILL_LIST = [
+    Skill('acrobatics', 'dex', True),
+    Skill('appraise', 'int', False),
+    Skill('bluff', 'cha', False),
+    Skill('climb', 'str', True),
+    Skill('craft', 'int', False),
+    Skill('disable-device', 'dex', False),
+    Skill('diplomacy', 'cha', False),
+    Skill('disguise', 'cha', False),
+    Skill('escape-artist', 'dex', True),
+    Skill('fly', 'dex', True),
+    Skill('handle-animal', 'cha', False),
+    Skill('heal', 'wis', False),
+    Skill('intimidate', 'cha', False),
+    Skill('knowledge', 'int', False),
+    Skill('linguistics', 'int', False),
+    Skill('perception', 'wis', False),
+    Skill('perform', 'cha', False),
+    Skill('profession', 'wis', False),
+    Skill('ride', 'dex', True),
+    Skill('sense-motive', 'wis', False),
+    Skill('sleight-of-hand', 'dex', True),
+    Skill('spellcraft', 'int', False),
+    Skill('stealth', 'dex', True),
+    Skill('survival', 'wis', False),
+    Skill('swim', 'str', True),
+    Skill('use-magic-device', 'cha', False),
+]
+
+SkillBlock = namedtuple('SkillBlock', [x.name for x in SKILL_LIST])
 
 KNOWLEDGE_LIST = (
     'arcana dungeoneering engineering geography history local nature nobility'
