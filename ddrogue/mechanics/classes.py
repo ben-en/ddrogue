c19635578fc8e0b27e8d06f122e765b363aba963
Ben Ennis <nwbenennis@gmail.com>
2016-10-09 11:03:05 -0700

    use namedtuples instead of class objects for character classes

--- ddrogue/mechanics/classes.py
+++ ddrogue/mechanics/classes.py
@@ -1,20 +1,29 @@
+from collections import namedtuple
+
 from .bab import HIGH_BAB
 from .saves import GOOD_SAVE, BAD_SAVE, compile_saves
 
 
-class Fighter(object):
-    def __init__(self):
-        self.class_skills = 'climb craft handle animal intimidate '.split() + \
-                            'knowledge profession ride survival swim'.split()
-        self.knowledges = 'dungeoneering engineering'.split()
+BaseClass = namedtuple('BaseClass', [
+    'hd',
+    'bab',
+    'saves',
+    'skills',
+    'knowledges',
+    'sp',
+    'gold',
+    'features'
+])
 
-        self.hd = '1d10'
-        self.bab = HIGH_BAB
-        self.saves = compile_saves(GOOD_SAVE, BAD_SAVE, BAD_SAVE)
-        self.skill_progression = 2  # implies "int+2"
-        self.base_gold = '5d6'  # implies roll(5d6) * 10gp
 
-    def init_specials(self):
-        """ Should add special abilities, eg fighter's bonus feat every level
-        """
-        pass
+Fighter = BaseClass(
+    '1d10',
+    HIGH_BAB,
+    compile_saves(GOOD_SAVE, BAD_SAVE, BAD_SAVE),
+    ('climb craft handle animal intimidate knowledge profession ride survival'
+     'swim').split(),
+    'dungeoneering engineering'.split(),
+    2,  # implies intelligence + 2 skill progression
+    '5d6',
+    [{} for x in range(20)]
+)
