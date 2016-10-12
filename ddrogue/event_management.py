1f8fd7a23b776d8aab767e37a97abe0826b740d6
Ben Ennis <nwbenennis@gmail.com>
2016-10-11 20:31:16 -0700

    make move_to character agnostic, add step limit

--- ddrogue/event_management.py
+++ ddrogue/event_management.py
@@ -83,13 +83,13 @@ def move_right(state, event):
     return state
 
 
-def move_to(state, pos):
+def move_to(state, char, pos, steps=-1):
     path = astar(state.map.floor.transpose(),
-                 tuple(state.map.grid_pos(state.player.pos)),
+                 tuple(state.map.grid_pos(char.pos)),
                  tuple(state.map.grid_pos(pos)))
     path.reverse()
-    for step in path:
-        state.player.pos = state.map.pixel_pos(step)
+    for step in path[:steps]:
+        char.pos = state.map.pixel_pos(step)
         state.draw()
         sleep(0.1)
     return state
@@ -110,7 +110,7 @@ def process_key_press(state, event):
 
 
 def process_click(state, event):
-    move_to(state, event.pos)
+    move_to(state, state.player, event.pos)
     return state
 
 
