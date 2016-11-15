99f489640bb4f6090d7c827dda7a920f832eda99
Ben Ennis <nwbenennis@gmail.com>
2016-11-15 10:24:52 -0800

    find the module location for files

--- ddrogue/constants.py
+++ ddrogue/constants.py
@@ -1,3 +1,5 @@
+from os import getcwd
+from os.path import realpath, join, dirname
 import re
 
 import pygame
@@ -5,6 +7,7 @@ import pygame
 
 pygame.init()
 pygame.font.init()
+
 
 # Set up the screen
 # SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -17,9 +20,11 @@ FONT = pygame.font.SysFont(FONT_NAME, FONT_SIZE)
 UI_SIZE = 400
 
 # TODO relative file paths
-KEYMAP_FILE = './controls.json'
-SAVE_DIR = './saves'
+__location__ = realpath(join(getcwd(), dirname(__file__)))
+KEYMAP_FILE = join(__location__, 'controls.json')
+SAVE_DIR = join(__location__, 'saves')
 
+# Unused
 MOVEMENT_EVENTS = 'UP DOWN LEFT RIGHT'.split()
 
 ALPHA_RE = re.compile("[a-zA-Z0-9]")
