f243d386889722058e6ec156b208ed55214ea79c
Ben Ennis <nwbenennis@gmail.com>
2016-12-13 21:53:14 -0800

    update EncounterState class

--- ddrogue/encounters/events.py
+++ ddrogue/encounters/events.py
@@ -7,11 +7,12 @@ import pygame
 from pygame import draw
 from pygame import Rect
 
-from ..colors import BLACK, LIGHT_BLUE, GREY
-from ..constants import UI_SIZE, SAVE_DIR, SCREEN
-from ..ui import menu
+from ..colors import BLACK, LIGHT_BLUE, GREY, WHITE
+from ..constants import UI_SIZE, SAVE_DIR, SCREEN, FONT
+from ..ui import menu, select_loop, Selectable
 from ..mechanics.dice import roll
-from .combat import move, move_to, charge
+from .combat import (move_to, FULL_ROUND_ACTIONS, STANDARD_ACTIONS,
+                     MOVE_ACTIONS, SWIFT_ACTIONS, FREE_ACTIONS)
 from .map import EncounterMap
 from .ui import StatusBox, HUD
 
@@ -242,7 +243,7 @@ def ability(state, event):
     ability_list = map(lambda x: x.__name__, state.char.features['active'])
     ability = menu(ability_list, xy=state.map.ui_size)
     if not ability:
-        state._print('No event selected')
+        state._print('No ability selected')
         return
     res = state.char.features['active'][ability_list.index(ability)]
     return res(state)
@@ -285,12 +286,66 @@ EVENT_MAP = {
 }
 
 
+def interact_with_user(state):
+    """
+    Get an event, returning the event if it's unrelated to the UI.
+    """
+    # Wait for an event
+    event = pygame.event.wait()
+    if event.type == pygame.KEYUP:
+        print('keycode: ', event.key)
+        try:
+            print('key: ', chr(event.key))
+        except ValueError as e:
+            print(e)
+        if event.key == ord('\t'):  # tab
+            # switch to the next ui element
+            state.selected_element += 1
+            if state.selected_element >= len(state.ui):
+                state.selected_element = 0
+            elif state.selected_element < 0:
+                state.selected_element = len(state.ui) - 1
+            return
+        elif event.key in [ord(c) for c in '\n ']:  # space /enter
+            state.output.fullscreen()
+        elif event.key == 275:  # right
+            state.hud.adjust_selected_player(1)
+        elif event.key == 276:  # left
+            state.hud.adjust_selected_player(-1)
+    elif event.type in [pygame.MOUSEMOTION, pygame.MOUSEBUTTONUP]:
+        if state.output.area_rect.collidepoint(event.pos):
+            print('output box collision', state.output.rect)
+            state.output.fullscreen()
+        elif state.hud.area_rect.collidepoint(event.pos):
+            print('hud collision', state.hud.rect)
+            for index in range(len(state.hud.elements)):
+                if state.hud.element_areas[index].collidepoint(event.pos):
+                    print('clicked on ', index)
+                    state.hud.selected_element = index
+                    func = state.hud.element_functions.get(index, None)
+                    if func:
+                        return func(state, event)
+        else:
+            # Pass the mouse event back if it was on the map
+            return event
+        # Don't return the even tif it was in the hud or output
+        return
+    # Removed event checks because they just return the event
+    # if event.type == pygame.KEYUP:
+    #     if event.key == 27:  # esc
+    #         # break out returning a quit event
+    #         return event
+    # elif event.type == pygame.QUIT:
+    #     return event
+    return event
+
+
 def player_turn(state):
     done = 0
     while not done:
         # Wait for an event
         while 1:
-            event = state.interact_with_user()
+            event = interact_with_user(state)
             if event:
                 break
         if event == 'done':
@@ -345,13 +400,16 @@ class EncounterState:
         # UI
         set_events()
         s_width, s_height = SCREEN.get_size()
-        self.map = EncounterMap(self.actors, floor_plan,
-                                (s_width - UI_SIZE, s_height - UI_SIZE / 2))
-        self.hud = HUD(self, self.players, (s_width - UI_SIZE, 0),
-                       UI_SIZE, s_height)
-        self.output = StatusBox(self,
-                                (0, s_height - UI_SIZE / 2), s_width - UI_SIZE,
-                                UI_SIZE / 2)
+        self.map = EncounterMap(
+            self.actors, floor_plan,
+            (s_width - UI_SIZE, s_height - UI_SIZE / 2)
+        )
+        self.hud = HUD(
+            self, self.players, (s_width - UI_SIZE, 0), UI_SIZE, s_height
+        )
+        self.output = StatusBox(
+            self, (0, s_height - UI_SIZE / 2), s_width - UI_SIZE, UI_SIZE / 2
+        )
         # Assign _print method to self for easy access
         self._print = self.output._print
         self.ui = [self.map, self.hud, self.output]
@@ -360,11 +418,8 @@ class EncounterState:
                             self.ui]
         self.selected_element = 0
 
+        # If the first character to act is a pc, this is necessary
         self.update_player_ui()
-
-        # Set the selected player (if one exists)
-        if self.actors[0] in self.players:
-            self.hud.selected_player = self.hud.players.index(self.char)
 
     @property
     def char(self):
@@ -374,74 +429,19 @@ class EncounterState:
         SCREEN.fill(BLACK)
         # Draw the panels on the screen
         self.map.update(self.char.pos)
-        self.hud.update()
         for e in self.ui:
             SCREEN.blit(e.img, e.pos)
         draw.rect(SCREEN, GREY, Rect((0, 0), self.map.ui_size), 2)
         selected_element = self.ui[self.selected_element]
-        pygame.draw.rect(
+        draw.rect(
             SCREEN, LIGHT_BLUE,
             (selected_element.pos, selected_element.img.get_size()), 1
         )
         pygame.display.flip()
 
-    def interact_with_user(self):
-        """
-        Interact with the user (help dialogs etc) until a character acction
-        occurs.
-        """
-        # Draw the frame
-        self.draw()
-        # Wait for an event
-        event = pygame.event.wait()
-        if event.type == pygame.KEYUP:
-            print('keycode: ', event.key)
-            try:
-                print('key: ', chr(event.key))
-            except ValueError as e:
-                print(e)
-            if event.key == 27:  # esc
-                # break out returning a quit event
-                return event
-            elif event.key == ord('\t'):
-                # If tab, switch to the next ui element
-                self.selected_element += 1
-                if self.selected_element >= len(self.ui):
-                    self.selected_element = 0
-                elif self.selected_element < 0:
-                    self.selected_element = len(self.ui) - 1
-                return
-            elif event.key in [ord(c) for c in '\n ']:
-                self.output.fullscreen()
-            elif event.key == 275:
-                self.hud.adjust_selected_player(1)
-            elif event.key == 276:
-                self.hud.adjust_selected_player(-1)
-        elif event.type == pygame.QUIT:
-            return event
-        elif event.type in [pygame.MOUSEMOTION, pygame.MOUSEBUTTONUP]:
-            if self.output.area_rect.collidepoint(event.pos):
-                print('output box collision', self.output.rect)
-                self.output.fullscreen()
-            elif self.hud.area_rect.collidepoint(event.pos):
-                print('hud collision', self.hud.rect)
-                for index in range(len(self.hud.elements)):
-                    if self.hud.element_areas[index].collidepoint(event.pos):
-                        print('clicked on ', index)
-                        self.hud.selected_element = index
-                        func = self.hud.element_functions.get(index, None)
-                        if func:
-                            return func(self, event)
-            else:
-                # Pass the mouse event back if it was on the map
-                return event
-            # Don't return the even tif it was in the hud or output
-            return
-        return event
-
     def next_turn(self):
-        self.draw()
         self._print('%s\'s turn' % self.char.s)
+        self.draw()
         if len(self.char.effects):
             for e in self.char.effects.pop[0]:
                 e(self)
